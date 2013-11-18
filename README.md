727c780abc41800de09bc92a70625b1bf3453567
Joseph Swick <joseph.swick@meltwater.com>
2013-11-18 10:48:47 -0500

    Install and manage the Beanstalkd package and service

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ puppet-beanstalkd
 =================
 
 Puppet module for managing Beanstalkd
+
+This module manages the beanstalkd service on RedHat and CentOS
+
---
+++ manifests/init.pp
@@ -0,0 +1,55 @@
+#Class to manage beanstalkd on Redhat/Centos
+class beanstalkd (
+  $manage_package                 = true,
+  $manage_sysconfig               = true,
+  $package_name                   = 'beanstalkd',
+  $package_ensure                 = 'present',
+  $service_name                   = 'beanstalkd',
+  $service_enable                 = true,
+  $beanstalkd_addr                = '0.0.0.0',
+  $beanstalkd_port                = '11300',
+  $beanstalkd_user                = 'beanstalkd',
+  $beanstalkd_max_job_size        = undef,
+  $beanstalkd_binlog_dir          = undef,
+  $beanstalkd_binlog_fsync_period = undef,
+  $beanstalkd_binlog_size         = undef,
+) {
+  validate_bool($manage_package)
+  validate_string($package_name)
+  validate_string($package_ensure)
+  validate_string($service_name)
+  validate_bool($service_enable)
+
+  $beanstalkd_cfg = '/etc/sysconfig/beanstalkd'
+
+  if $manage_package {
+    package { $package_name:
+      ensure => $package_ensure,
+      before => [Service[$service_name], File[$beanstalkd_cfg]],
+    }
+  }
+
+  if $package_ensure != 'absent' {
+    if $service_enable {
+      $service_ensure = 'running'
+    } else {
+      $service_ensure = 'stopped'
+    }
+
+    if $manage_sysconfig {
+      file { $beanstalkd_cfg:
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template("${module_name}/beanstalkd.erb"),
+        notify  => Service[$service_name],
+      }
+    }
+
+    service { $service_name:
+      ensure => $service_ensure,
+      enable => $service_enable
+    }
+  }
+}
